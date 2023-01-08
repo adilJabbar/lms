e0f39589a382327ff8497b5ff6e1d134bfb81eb2
nauman <nauman689@gmail.com>
2023-01-09 00:11:18 +0500

    subscribe added

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -26,23 +26,36 @@ class SubscriptionController extends Controller {
     }
 
     public function savePaymentDetails(Request $request) {
-        $data['user_id'] = Crypt::decrypt($request->user_id);
-        $data['subscription_id'] = Crypt::decrypt($request->subscription_id);
-        $data['subscription'] = \App\Models\Subscription::getSubscription($data['subscription_id']);
+        if (isset($request->user_id) && isset($request->subscription_id)) {
+            $data['user_id'] = Crypt::decrypt($request->user_id);
+            $data['subscription_id'] = Crypt::decrypt($request->subscription_id);
+            $data['subscription'] = \App\Models\Subscription::getSubscription($data['subscription_id']);
 
-        $prepareData = self::prepareData($request->toArray());
-
-        echo "<pre>";
-        print_r($subscription);
-        exit;
-        return view('payment-details', compact('data'));
+            $prepareData = self::prepareData($data);
+            $save = \App\Models\UserSubscribedPlan::saveData($prepareData);
+            if ($save) {
+                return redirect()->route('dashboard')->with('Subscribed Successfully!');
+            } else {
+                return redirect()->back()->with('Error occurred! please try again');
+            }
+        } else {
+            return redirect()->back()->with('Error occurred! please try again');
+        }
     }
 
-    public static function prepareData($request) {
-        $data = [];
-        if (!empty($request)) {
-            $data['']
+    public static function prepareData($subscriptionData) {
+        $data['subscription_id'] = $subscriptionData['subscription']['id'];
+        $data['price'] = $subscriptionData['subscription']['price'];
+        $data['user_id'] = $subscriptionData['user_id'];
+        $data['subscription_start_date'] = date('Y-m-d H:i:s');
+        if (strtolower($subscriptionData['subscription']['plans']) == 'yearly' || strtolower($subscriptionData['subscription']['plans']) == 'anually') {
+            $data['subscription_end_date'] = date('Y-m-d H:i:s', strtotime($data['subscription_start_date'] . ' + 1 years'));
+        } elseif (strtolower($subscriptionData['subscription']['plans']) == 'monthly') {
+            $data['subscription_end_date'] = date('Y-m-d H:i:s', strtotime($data['subscription_start_date'] . ' + 1 months'));
+        } elseif (strtolower($subscriptionData['subscription']['plans']) == 'weekly') {
+            $data['subscription_end_date'] = date('Y-m-d H:i:s', strtotime($data['subscription_start_date'] . ' + 1 weeks'));
         }
+        return $data;
     }
 
 }
--- database/migrations/2023_01_08_122506_create_user_subscribed_plans_table.php
+++ database/migrations/2023_01_08_122506_create_user_subscribed_plans_table.php
@@ -4,17 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
+
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('user_subscribed_plans', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('subscription_id')->constrained('subscriptions');
+            $table->double('price')->comment('price of the subscription paid by customer');
+            $table->timestamp('subscription_start_date')->comment('datetime when customer subscribes');
+            $table->timestamp('subscription_end_date')->comment('datetime when customer subscription ends');
+            $table->enum('paid_with', ['credit_card', 'paypal'])->default('credit_card');
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
@@ -24,8 +30,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('user_subscribed_plans');
     }
 };
